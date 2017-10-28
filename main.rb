676f6606be952ef17285148f7aca81b12a8cbfea
jonahthompson <jonahthompson12@gmail.com>
2017-10-28 12:38:53 -0600

    added erbs

--- main.rb
+++ main.rb
@@ -1,6 +1,17 @@
 require 'sinatra'
 
-	get '/' do
-		erb :home
-	end
+get '/' do
+	erb :home
+end
+
+get '/contact_us' do
+	erb :contact_us
+end
+	
+post '/thanks' do
+	p params
+	@email = params[:email]
+	@message = params[:message]
+	erb :thanks
+end
 	
