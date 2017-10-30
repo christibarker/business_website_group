eddfe4a6728e437836a855a9bd15c1788f7da455
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-30 15:34:39 -0600

    services layout and javascript

---
+++ public/js/main.js
@@ -0,0 +1,84 @@
+
+
+$('.add').on('click', function(){
+			$('#shopping').text('hi');
+			$('#cost').text('$' + 300);
+		});
+
+$('.add_one').on('click', function(){
+			$('#shopping_two').text('hi 2');
+			$('#cost').text('$' + 300);
+		});
+
+$('.add_two').on('click', function(){
+			$('#shopping_three').text('hi 3');
+			$('#cost').text('$' + 300);
+		});
+
+$('.add_three').on('click', function(){
+			$('#shopping_four').text('hi 4');
+			$('#cost').text('$' + 300);
+		});
+
+$('.remove').on('click', function(){
+			$('#shopping').text('hi');
+			$('#cost').text('');
+		});
+
+$('.remove_one').on('click', function(){
+			$('#shopping_two').text('hi 2');
+			$('#cost').text('');
+		});
+
+$('.remove_two').on('click', function(){
+			$('#shopping_three').text('hi 3');
+			$('#cost').text('');
+		});
+
+$('.remove_three').on('click', function(){
+			$('#shopping_four').text('hi 4');
+			$('#cost').text('');
+		});
+
+// class Cost
+// 	attr_accessor :cost
+// 	def	initialize(args)
+// 		@cost = args
+// 	end
+
+// 	def	add()
+// 		if @cost 
+// 		@cost += 
+
+// 	end 
+
+// 	def	minus()
+// 		if @cost 
+// 		@cost -= 
+
+// 	end 
+
+// var cost = Cost.new()
+
+// class Elevator
+// 	attr_accessor :floor
+// 	def initialize(args)
+// 		@floor = args
+// 	end
+// 	def go_up()
+// 		if @floor < 12
+// 			@floor += 1
+// 		end
+// 	end
+// 	def go_down()
+// 		if @floor > 1
+// 			@floor -= 1 
+// 		end
+// 	end
+// 	def greeting()
+// 		puts "welcome to the #{floor} floor"
+// 	end
+// end
+
+
+
